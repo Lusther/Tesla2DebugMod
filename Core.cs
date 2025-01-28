6a194e95e8ffdfebcaddc167de1852d2817ac4a3
Lusther <l9.lemaire@gmail.com>
2025-01-28 17:06:26 +0100

    Add debugMove and SkillMenu features

--- Core.cs
+++ Core.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 using Il2Cpp;
 using UnityEngine.InputSystem;
 using System.Collections;
+using static Il2Cpp.Interop;
+using Il2CppSystem.Security.Cryptography;
 
-[assembly: MelonInfo(typeof(Tesla2DebugMod.Core), "Tesla2DebugMod", "1.0.0", "Lusther", null)]
+[assembly: MelonInfo(typeof(Tesla2DebugMod.Core), "Tesla2DebugMod", "1.1.0", "Lusther", null)]
 [assembly: MelonGame("Rain", "Teslagrad 2")]
 
 namespace Tesla2DebugMod
@@ -12,10 +14,13 @@ namespace Tesla2DebugMod
     public class Core : MelonMod
     {
 
-        private InputAction savePosAction;
-        private InputAction loadPosAction;
-        private InputAction saveSlotAction;
-        private InputAction loadSlotAction;
+        private static InputAction savePosAction;
+        private static InputAction loadPosAction;
+        private static InputAction saveSlotAction;
+        private static InputAction loadSlotAction;
+        private static InputAction toggleDebugMoveAction;
+        private static InputAction debugMoveAction;
+        private static InputAction menuAction;
 
 
         private static Vector2 savedPos;
@@ -26,6 +31,10 @@ namespace Tesla2DebugMod
 
         private static SaveDataSlot currentSaveDataSlot;
         private static SaveDataSlot savedSaveDataSlot;
+
+        private static AbilityMenu abilityMenu;
+
+        private static bool isDebugMoveActivated;
 
         public override void OnInitializeMelon()
         {
@@ -42,7 +51,25 @@ namespace Tesla2DebugMod
             loadSlotAction = new InputAction("LoadSlot", binding: "<Keyboard>/F4");
             loadSlotAction.Enable();
 
+            toggleDebugMoveAction = new InputAction("ToggleDebugMove", binding: "<Keyboard>/F5");
+            toggleDebugMoveAction.Enable();
+
+            debugMoveAction = new InputAction("DebugMove");
+            debugMoveAction.AddCompositeBinding("2DVector")
+                .With("Up", "<Keyboard>/w")
+                .With("Down", "<Keyboard>/s")
+                .With("Left", "<Keyboard>/a")
+                .With("Right", "<Keyboard>/d");
+            debugMoveAction.Enable();
+
+            menuAction = new InputAction("Menu", binding: "<Keyboard>/F6");
+            menuAction.Enable();
+
             notificationString = "";
+
+            abilityMenu = new AbilityMenu();
+            
+            isDebugMoveActivated = false;
 
             LoggerInstance.Msg("Initialized.");
         }
@@ -68,12 +95,27 @@ namespace Tesla2DebugMod
             {
                 LoadSlot();
             }
+
+            if (menuAction.WasPerformedThisFrame())
+            {
+                abilityMenu.ToggleWindow();
+            }
+
+            if (toggleDebugMoveAction.WasPerformedThisFrame())
+            {
+                ToggleDebugMove();
+            }
+
+            HandleDebugMove();
+            abilityMenu.OnLateUpdate(); 
         }
 
         public override void OnGUI()
         {
             GUI.Label(new Rect(20, 20, 1000, 50), "Debug Mod activated: any run done with it is invalid");
             GUI.Label(new Rect(20, 50, 1000, 50), notificationString);
+
+            abilityMenu.OnGUI();
         }
 
         private static void savePos()
@@ -123,120 +165,48 @@ namespace Tesla2DebugMod
 
         public static void SaveSlot()
         {
+            Melon<Tesla2DebugMod.Core>.Logger.Msg("Saving Slot");
+            DrawText("Saving Slot");
+
             currentSaveDataSlot = Game.activeSlot;
             savedSaveDataSlot = currentSaveDataSlot.DeepCopy();
         }
 
         public static void LoadSlot()
         {
+            Melon<Tesla2DebugMod.Core>.Logger.Msg("Loading Slot");
+            DrawText("Loading Slot");
+
             savedSaveDataSlot.CopyTo(currentSaveDataSlot);
         }
-    }
-}
 
-
-
-public static class SaveDataSlotExtensions
-{
-    public static SaveDataSlot DeepCopy(this SaveDataSlot obj)
-    {
-        if (obj == null)
-            return default;
-
-        SaveDataSlot copy = new SaveDataSlot();
-        obj.CopyTo(copy);
-        return copy;
-    }
-
-    public static void CopyTo(this SaveDataSlot from, SaveDataSlot to)
-    {
-        to.activitiesUnlocked = new Il2CppSystem.Collections.Generic.List<int>(from.activitiesUnlocked.Cast<Il2CppSystem.Collections.Generic.IEnumerable<int>>());
-        to.axeUnlocked = from.axeUnlocked;
-        to.blinkUnlocked = from.blinkUnlocked;
-        to.blinkWireAxeUnlocked = from.blinkWireAxeUnlocked;
-        to.cloakUnlocked = from.cloakUnlocked;
-        to.dateModified = from.dateModified;
-        to.doubleJumpUnlocked = from.doubleJumpUnlocked;
-        to.fafnirBossFightBeaten = from.fafnirBossFightBeaten;
-        to.galvanBossFightBeaten = from.galvanBossFightBeaten;
-        to.gameWasCompletedOnce = from.gameWasCompletedOnce;
-        to.halvtannBossFightBeaten = from.halvtannBossFightBeaten;
-        to.hasMetGalvan = from.hasMetGalvan;
-        to.hulderBossfightBeaten = from.hulderBossfightBeaten;
-        to.HulderUnderworldChaseProgression = from.HulderUnderworldChaseProgression;
-        to.hulder_DarkRoomLevelChaseDone = from.hulder_DarkRoomLevelChaseDone;
-        to.hulder_GrueEyesLevelChaseDone = from.hulder_GrueEyesLevelChaseDone;
-        to.hulder_PreBossDiscoverTraversed = from.hulder_PreBossDiscoverTraversed;
-        to.hulder_PreBossLevelChaseDone = from.hulder_PreBossLevelChaseDone;
-        to.invasionSequenceDone = from.invasionSequenceDone;
-        to.mapShapesUnlocked = from.mapShapesUnlocked;
-        to.mapShapesUnlocked = new Il2CppSystem.Collections.Generic.List<String>(from.mapShapesUnlocked.Cast<Il2CppSystem.Collections.Generic.IEnumerable<String>>());
-        to.mapUnlocked = from.mapUnlocked;
-        to.mjolnirUnlocked = from.mjolnirUnlocked;
-        to.mooseBossFightBeaten = from.mooseBossFightBeaten;
-        to.name = from.name;
-        to.omniBlinkUnlocked = from.omniBlinkUnlocked;
-        to.powerSlideUnlocked = from.powerSlideUnlocked;
-        to.redCloakUnlocked = from.redCloakUnlocked;
-        to.respawnFacingRight = from.respawnFacingRight;
-        to.respawnPoint = from.respawnPoint;
-        to.respawnScene = from.respawnScene;
-        to.savedCharges = new Il2CppSystem.Collections.Generic.List<SavedCharge>();
-        foreach (SavedCharge item in from.savedCharges)
+        public static void ToggleDebugMove()
         {
-            to.savedCharges.Add(item.DeepCopy());
+            isDebugMoveActivated = !isDebugMoveActivated;
+
+            Melon<Tesla2DebugMod.Core>.Logger.Msg($"ToggleDebugMove : {isDebugMoveActivated}");
+            DrawText($"ToggleDebugMove : {isDebugMoveActivated}");
+
+            Player.instance.localBody.isKinematic = isDebugMoveActivated;
+            Player.instance.movement.enabled = !isDebugMoveActivated;
+            Player.MainCollider.enabled = !isDebugMoveActivated;
+            Player.DeathHandler.deathCollider.enabled = !isDebugMoveActivated;
         }
-        to.savedResetInfos = new Il2CppSystem.Collections.Generic.List<SavedResetInfo>();
-        foreach (SavedResetInfo item in from.savedResetInfos)
+
+        public static void HandleDebugMove()
         {
-            to.savedResetInfos.Add(item.DeepCopy());
+            if (isDebugMoveActivated)
+            {
+                try
+                {
+                    Vector2 inputAxe = debugMoveAction.ReadValue<Vector2>();
+                    Player.instance.transform.position += new Vector3(inputAxe.x, inputAxe.y, 0) * 0.05f;
+                }
+                catch (InvalidOperationException)
+                {
+                    Melon<Tesla2DebugMod.Core>.Logger.Msg("The given TValue type does not match the value type of the control or composite currently driving the action.");
+                }
+            }
         }
-        to.scrollsPickedUp = new Il2CppSystem.Collections.Generic.List<int>(from.scrollsPickedUp.Cast<Il2CppSystem.Collections.Generic.IEnumerable<int>>());
-        to.scrollsSeenInCollection = new Il2CppSystem.Collections.Generic.List<int>(from.scrollsSeenInCollection.Cast<Il2CppSystem.Collections.Generic.IEnumerable<int>>());
-        to.secretsMapUnlocked = from.secretsMapUnlocked;
-        to.timeSpent = from.timeSpent;
-        to.triggersSet = new Il2CppSystem.Collections.Generic.List<String>(from.triggersSet.Cast<Il2CppSystem.Collections.Generic.IEnumerable<String>>());
-        to.trollMiniBossFightBeaten = from.trollMiniBossFightBeaten;
-        to.version = from.version;
-        to.vikingBlimpOnTheHunt = from.vikingBlimpOnTheHunt;
-        to.vikingBlimpPosition = from.vikingBlimpPosition;
-        to.waterblinkUnlocked = from.waterblinkUnlocked;
-    }
-
-    public static void CopyTo(this SavedCharge from, SavedCharge to)
-    {
-        to.saveID = from.saveID;
-        to.charge = from.charge;
-    }
-
-    public static SavedCharge DeepCopy(this SavedCharge obj)
-    {
-        if (obj == null)
-            return default;
-
-        SavedCharge copy = new SavedCharge();
-        obj.CopyTo(copy);
-        return copy;
-    }
-
-    public static void CopyTo(this SavedResetInfo from, SavedResetInfo to)
-    {
-        to.saveID = from.saveID;
-        to.position = from.position;
-        to.rotationEuler = from.rotationEuler;
-        to.scale = from.scale;
-        to.isKinematic = from.isKinematic;
-        to.angularDrag = from.angularDrag;
-        to.drag = from.drag;
-    }
-
-    public static SavedResetInfo DeepCopy(this SavedResetInfo obj)
-    {
-        if (obj == null)
-            return default;
-
-        SavedResetInfo copy = new SavedResetInfo();
-        obj.CopyTo(copy);
-        return copy;
     }
 }
---
+++ SaveDataSlotExtensions.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Il2Cpp;
+
+namespace Tesla2DebugMod
+{
+    public static class SaveDataSlotExtensions
+    {
+        public static SaveDataSlot DeepCopy(this SaveDataSlot obj)
+        {
+            if (obj == null)
+                return default;
+
+            SaveDataSlot copy = new SaveDataSlot();
+            obj.CopyTo(copy);
+            return copy;
+        }
+
+        public static void CopyTo(this SaveDataSlot from, SaveDataSlot to)
+        {
+            to.activitiesUnlocked = new Il2CppSystem.Collections.Generic.List<int>(from.activitiesUnlocked.Cast<Il2CppSystem.Collections.Generic.IEnumerable<int>>());
+            to.axeUnlocked = from.axeUnlocked;
+            to.blinkUnlocked = from.blinkUnlocked;
+            to.blinkWireAxeUnlocked = from.blinkWireAxeUnlocked;
+            to.cloakUnlocked = from.cloakUnlocked;
+            to.dateModified = from.dateModified;
+            to.doubleJumpUnlocked = from.doubleJumpUnlocked;
+            to.fafnirBossFightBeaten = from.fafnirBossFightBeaten;
+            to.galvanBossFightBeaten = from.galvanBossFightBeaten;
+            to.gameWasCompletedOnce = from.gameWasCompletedOnce;
+            to.halvtannBossFightBeaten = from.halvtannBossFightBeaten;
+            to.hasMetGalvan = from.hasMetGalvan;
+            to.hulderBossfightBeaten = from.hulderBossfightBeaten;
+            to.HulderUnderworldChaseProgression = from.HulderUnderworldChaseProgression;
+            to.hulder_DarkRoomLevelChaseDone = from.hulder_DarkRoomLevelChaseDone;
+            to.hulder_GrueEyesLevelChaseDone = from.hulder_GrueEyesLevelChaseDone;
+            to.hulder_PreBossDiscoverTraversed = from.hulder_PreBossDiscoverTraversed;
+            to.hulder_PreBossLevelChaseDone = from.hulder_PreBossLevelChaseDone;
+            to.invasionSequenceDone = from.invasionSequenceDone;
+            to.mapShapesUnlocked = from.mapShapesUnlocked;
+            to.mapShapesUnlocked = new Il2CppSystem.Collections.Generic.List<String>(from.mapShapesUnlocked.Cast<Il2CppSystem.Collections.Generic.IEnumerable<String>>());
+            to.mapUnlocked = from.mapUnlocked;
+            to.mjolnirUnlocked = from.mjolnirUnlocked;
+            to.mooseBossFightBeaten = from.mooseBossFightBeaten;
+            to.name = from.name;
+            to.omniBlinkUnlocked = from.omniBlinkUnlocked;
+            to.powerSlideUnlocked = from.powerSlideUnlocked;
+            to.redCloakUnlocked = from.redCloakUnlocked;
+            to.respawnFacingRight = from.respawnFacingRight;
+            to.respawnPoint = from.respawnPoint;
+            to.respawnScene = from.respawnScene;
+            to.savedCharges = new Il2CppSystem.Collections.Generic.List<SavedCharge>();
+            foreach (SavedCharge item in from.savedCharges)
+            {
+                to.savedCharges.Add(item.DeepCopy());
+            }
+            to.savedResetInfos = new Il2CppSystem.Collections.Generic.List<SavedResetInfo>();
+            foreach (SavedResetInfo item in from.savedResetInfos)
+            {
+                to.savedResetInfos.Add(item.DeepCopy());
+            }
+            to.scrollsPickedUp = new Il2CppSystem.Collections.Generic.List<int>(from.scrollsPickedUp.Cast<Il2CppSystem.Collections.Generic.IEnumerable<int>>());
+            to.scrollsSeenInCollection = new Il2CppSystem.Collections.Generic.List<int>(from.scrollsSeenInCollection.Cast<Il2CppSystem.Collections.Generic.IEnumerable<int>>());
+            to.secretsMapUnlocked = from.secretsMapUnlocked;
+            to.timeSpent = from.timeSpent;
+            to.triggersSet = new Il2CppSystem.Collections.Generic.List<String>(from.triggersSet.Cast<Il2CppSystem.Collections.Generic.IEnumerable<String>>());
+            to.trollMiniBossFightBeaten = from.trollMiniBossFightBeaten;
+            to.version = from.version;
+            to.vikingBlimpOnTheHunt = from.vikingBlimpOnTheHunt;
+            to.vikingBlimpPosition = from.vikingBlimpPosition;
+            to.waterblinkUnlocked = from.waterblinkUnlocked;
+        }
+
+        public static void CopyTo(this SavedCharge from, SavedCharge to)
+        {
+            to.saveID = from.saveID;
+            to.charge = from.charge;
+        }
+
+        public static SavedCharge DeepCopy(this SavedCharge obj)
+        {
+            if (obj == null)
+                return default;
+
+            SavedCharge copy = new SavedCharge();
+            obj.CopyTo(copy);
+            return copy;
+        }
+
+        public static void CopyTo(this SavedResetInfo from, SavedResetInfo to)
+        {
+            to.saveID = from.saveID;
+            to.position = from.position;
+            to.rotationEuler = from.rotationEuler;
+            to.scale = from.scale;
+            to.isKinematic = from.isKinematic;
+            to.angularDrag = from.angularDrag;
+            to.drag = from.drag;
+        }
+
+        public static SavedResetInfo DeepCopy(this SavedResetInfo obj)
+        {
+            if (obj == null)
+                return default;
+
+            SavedResetInfo copy = new SavedResetInfo();
+            obj.CopyTo(copy);
+            return copy;
+        }
+    }
+}
