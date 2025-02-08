6a155efb2482047823733fc0865be89fb6bca276
Lusther <l9.lemaire@gmail.com>
2025-02-08 16:54:57 +0100

    v1.2.0 Add Training Tool and Debug View Coords

--- Core.cs
+++ Core.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 using Il2Cpp;
 using UnityEngine.InputSystem;
 using System.Collections;
-using static Il2Cpp.Interop;
-using Il2CppSystem.Security.Cryptography;
 
-[assembly: MelonInfo(typeof(Tesla2DebugMod.Core), "Tesla2DebugMod", "1.1.0", "Lusther", null)]
+[assembly: MelonInfo(typeof(Tesla2DebugMod.Core), "Tesla2DebugMod", "1.2.0", "Lusther", null)]
 [assembly: MelonGame("Rain", "Teslagrad 2")]
 
 namespace Tesla2DebugMod
 {
     public class Core : MelonMod
     {
+        private Rect warningLabel = new(20, 20, 1000, 50);
+        private Rect notificationLabel = new(20, 50, 1000, 50);
+        private Rect positionLabel = new(1600, 20, 1000, 50);
+        private Rect velocityLabel = new(1600, 50, 1000, 50);
+        private Rect graphRect = new Rect(1500, 80, 300, 200);
 
         private static InputAction savePosAction;
         private static InputAction loadPosAction;
@@ -21,7 +24,12 @@ namespace Tesla2DebugMod
         private static InputAction toggleDebugMoveAction;
         private static InputAction debugMoveAction;
         private static InputAction menuAction;
+        private static InputAction trainingToolMenuAction;
+        private static InputAction startTrainingTrial;
+        private static InputAction showDebugInfoAction;
 
+        private List<float> velocityValues = new List<float>();
+        private const int maxGraphPoints = 2000;
 
         private static Vector2 savedPos;
 
@@ -35,6 +43,8 @@ namespace Tesla2DebugMod
         private static AbilityMenu abilityMenu;
 
         private static bool isDebugMoveActivated;
+        private static bool isTrainingMenuShowed;
+        private static bool isDebugInfoShowed;
 
         public override void OnInitializeMelon()
         {
@@ -65,11 +75,22 @@ namespace Tesla2DebugMod
             menuAction = new InputAction("Menu", binding: "<Keyboard>/F6");
             menuAction.Enable();
 
+            trainingToolMenuAction = new InputAction("TrainingToolMenu", binding: "<Keyboard>/F7");
+            trainingToolMenuAction.Enable();
+
+            startTrainingTrial = new InputAction("StartTrainingTrial", binding: "<Keyboard>/F8");
+            startTrainingTrial.Enable();
+
+            showDebugInfoAction = new InputAction("ShowDebugInfo", binding: "<Keyboard>/F9");
+            showDebugInfoAction.Enable();
+
             notificationString = "";
 
             abilityMenu = new AbilityMenu();
             
             isDebugMoveActivated = false;
+            isTrainingMenuShowed = false;
+            isDebugInfoShowed = false;
 
             LoggerInstance.Msg("Initialized.");
         }
@@ -106,16 +127,61 @@ namespace Tesla2DebugMod
                 ToggleDebugMove();
             }
 
-            HandleDebugMove();
-            abilityMenu.OnLateUpdate(); 
+            if (trainingToolMenuAction.WasPerformedThisFrame())
+            {
+                isTrainingMenuShowed = !isTrainingMenuShowed;
+            }
+
+            if (startTrainingTrial.WasPerformedThisFrame())
+            {
+                MelonCoroutines.Start(TrainingTool.StartTrainingTrial());
+            }
+
+            if (showDebugInfoAction.WasPerformedThisFrame())
+            {
+                isDebugInfoShowed = !isDebugInfoShowed;
+            }
+
+
+
+            if (Player.instance is not null)
+            {
+                if (!Player.instance.Cast<UnityEngine.Object>().HasBeenDestroyed())
+                {
+                    Melon<Tesla2DebugMod.Core>.Logger.Msg($"{Player.instance}");
+                    HandleDebugMove();
+                    abilityMenu.OnLateUpdate();
+                    TrainingTool.OnLateUpdate();
+                    UpdateGraph(Math.Abs(getVelocity().x));
+                }
+            }
         }
 
         public override void OnGUI()
         {
-            GUI.Label(new Rect(20, 20, 1000, 50), "Debug Mod activated: any run done with it is invalid");
-            GUI.Label(new Rect(20, 50, 1000, 50), notificationString);
+            GUI.Label(warningLabel, "Debug Mod activated: any run done with it is invalid");
+            GUI.Label(notificationLabel, notificationString);
 
-            abilityMenu.OnGUI();
+            if (Player.instance is not null)
+            {
+                if (!Player.instance.Cast<UnityEngine.Object>().HasBeenDestroyed())
+                {
+                    abilityMenu.OnGUI();
+                    TrainingTool.OnGUI();
+                    if (isTrainingMenuShowed)
+                    {
+                        TrainingToolMenu.OnGUI();
+                    }
+
+                    if (isDebugInfoShowed)
+                    {
+                        GUI.Label(positionLabel, $"Position: {getPosition().x} | {getPosition().y}");
+                        GUI.Label(velocityLabel, $"Velocity: {getVelocity().x} | {getVelocity().y}");
+                        GUI.Box(graphRect, "Velocity Graph");
+                        DrawVelocityGraph(new Rect(10, 10, 300, 150));
+                    }
+                }
+            }
         }
 
         private static void savePos()
@@ -206,6 +272,75 @@ namespace Tesla2DebugMod
                 {
                     Melon<Tesla2DebugMod.Core>.Logger.Msg("The given TValue type does not match the value type of the control or composite currently driving the action.");
                 }
+            }
+        }
+
+        public static Vector2 getVelocity()
+        {
+            return Player.instance.localBody.velocity;
+        }
+
+        public static Vector2 getPosition()
+        {
+            return Player.instance.transform.position;
+        }
+
+        private void DrawVelocityGraph(Rect rect)
+        {
+            CreateLineMaterial();
+
+            if (velocityValues.Count < 2) return;
+
+            lineMaterial.SetPass(0);
+            GL.PushMatrix();
+            GL.Viewport(new Rect(graphRect.x, Screen.height - graphRect.y - graphRect.height, graphRect.width, graphRect.height));
+            GL.LoadOrtho();
+
+            GL.Begin(GL.LINES);
+            GL.Color(Color.green);
+
+            float maxVelocity = 30f;
+            for (int i = 1; i < velocityValues.Count; i++)
+            {
+                float x1 = (float)(i - 1) / maxGraphPoints;
+                float x2 = (float)i / maxGraphPoints;
+                float y1 = Mathf.Clamp01(velocityValues[i - 1] / maxVelocity);
+                float y2 = Mathf.Clamp01(velocityValues[i] / maxVelocity);
+
+                GL.Vertex(new Vector3(x1, y1, 0));
+                GL.Vertex(new Vector3(x2, y2, 0));
+            }
+
+            GL.End();
+            GL.PopMatrix();
+        }
+
+        private void UpdateGraph(float velocity)
+        {
+            velocityValues.Add(velocity);
+            if (velocityValues.Count > maxGraphPoints)
+            {
+                velocityValues.RemoveAt(0); // Keep the graph within the point limit
+            }
+        }
+
+        static Material lineMaterial;
+        static void CreateLineMaterial()
+        {
+            if (!lineMaterial)
+            {
+                // Unity has a built-in shader that is useful for drawing
+                // simple colored things.
+                Shader shader = Shader.Find("Hidden/Internal-Colored");
+                lineMaterial = new Material(shader);
+                lineMaterial.hideFlags = HideFlags.HideAndDontSave;
+                // Turn on alpha blending
+                lineMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                lineMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                // Turn backface culling off
+                lineMaterial.SetInt("_Cull", (int)UnityEngine.Rendering.CullMode.Off);
+                // Turn off depth writes
+                lineMaterial.SetInt("_ZWrite", 0);
             }
         }
     }
---
+++ TrainingToolMenu.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Il2Cpp;
+using UnityEngine;
+
+namespace Tesla2DebugMod
+{
+    internal static class TrainingToolMenu
+    {
+        public enum Direction
+        {
+            None,
+            N,
+            NE,
+            E,
+            SE,
+            S,
+            SO,
+            O,
+            NO,
+        }
+
+        private static Rect windowRect = new(50, 50, 300, 300);
+
+        public static Vector2 startingPos;
+        public static Vector2 endingPos;
+        public static Direction endingDirection = Direction.None;
+
+        private static Chrono chrono = new();
+
+        public static void OnGUI()
+        {
+            // Create the window
+            GUI.Window(0, windowRect, (UnityEngine.GUI.WindowFunction)DrawGridWindow, "Grid Window");
+        }
+
+        static void DrawGridWindow(int windowID)
+        {
+            if (GUI.Button(new Rect(20, 20, 100, 20), "Set Start Pos"))
+            {
+                startingPos = getPosition();
+            }
+            GUI.Label(new Rect(20, 40, 100, 20), "Starting Pos");
+            GUI.Label(new Rect(20, 60, 100, 20), $"x : {startingPos.x}");
+            GUI.Label(new Rect(20, 80, 100, 20), $"y : {startingPos.y}");
+
+            if (GUI.Button(new Rect(20, 100, 100, 20), "Set End Pos"))
+            {
+                endingPos = getPosition();
+            }
+            GUI.Label(new Rect(20, 120, 100, 20), "End pos");
+            GUI.Label(new Rect(20, 140, 100, 20), $"x : {endingPos.x}");
+            GUI.Label(new Rect(20, 160, 100, 20), $"y : {endingPos.y}");
+            GUI.Label(new Rect(20, 180, 100, 20), $"Direction : {endingDirection}");
+
+            if (GUI.Button(new Rect(20, 200, 20, 20), "↖"))
+            {
+                endingDirection = Direction.NO;
+            }
+            if (GUI.Button(new Rect(40, 200, 20, 20), "↑"))
+            {
+                endingDirection = Direction.N;
+            }
+            if (GUI.Button(new Rect(60, 200, 20, 20), "↗"))
+            {
+                endingDirection = Direction.NE;
+            }
+            if (GUI.Button(new Rect(20, 220, 20, 20), "←"))
+            {
+                endingDirection = Direction.O;
+            }
+            if (GUI.Button(new Rect(60, 220, 20, 20), "→"))
+            {
+                endingDirection = Direction.E;
+            }
+            if (GUI.Button(new Rect(20, 240, 20, 20), "↙"))
+            {
+                endingDirection = Direction.SO;
+            }
+            if (GUI.Button(new Rect(40, 240, 20, 20), "↓"))
+            {
+                endingDirection = Direction.S;
+            }
+            if (GUI.Button(new Rect(60, 240, 20, 20), "↘"))
+            {
+                endingDirection = Direction.SE;
+            }
+
+            if (GUI.Button(new Rect(20, 280, 20, 20), "L"))
+            {
+                PlayHandler playHandler = new PlayHandler();
+                playHandler.LoadGame(Game.activeSlot);
+            }
+
+            // Allow the window to be draggable
+            GUI.DragWindow();
+        }
+
+        static Vector2 getPosition()
+        {
+            return Player.instance.transform.position;
+        }
+    }
+}
